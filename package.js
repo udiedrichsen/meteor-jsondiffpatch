2fada722c9cf6fbadb287f295f4e809a697c5b35
Ulrich Diedrichsen <ulrich.diedrichsen@googlemail.com>
2015-11-24 11:43:33 +0100

    .

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-    name: 'risul:jsondiffpatch',
+    name: 'doedel:jsondiffpatch',
     summary: 'Meteor package for jsondiffpatch - Diff & patch JavaScript objects.',
     version: '1.0.4',
-    git: 'https://github.com/risul/meteor-jsondiffpatch'
+    git: 'https://github.com/udiedrichsen/meteor-jsondiffpatch'
 });
 
 Npm.depends({
-    jsondiffpatch: '0.1.33'
+    jsondiffpatch: '0.1.37'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('METEOR@0.9.2.2');
     api.export('jsondiffpatch');
-    api.addFiles('lib/jsondiffpatch.js', 'server');
+    api.addFiles('lib/jsondiffpatch.js', ['client','server']);
 });
